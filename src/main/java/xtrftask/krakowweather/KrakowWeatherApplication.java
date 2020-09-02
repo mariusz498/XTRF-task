f9fabada6ebd251657d74348f6488c372d649518
mariusz498 <hajdylamariusz@gmail.com>
2020-09-02 09:25:30 +0200

    first commit

---
+++ src/main/java/xtrftask/krakowweather/KrakowWeatherApplication.java
@@ -0,0 +1,13 @@
+package xtrftask.krakowweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrakowWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrakowWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xtrftask/krakowweather/KrakowWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package xtrftask.krakowweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrakowWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
